fb744cc2ea981c82cf6cdb35d1fe13a771215c20
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-08-29 17:03:31 +0530

    JS changes

---
+++ JS_Project1/js/main.js
@@ -0,0 +1,31 @@
+console.log("This is a JS file");
+//String
+let myVariable = "This is a document";
+console.log(typeof(myVariable));
+console.log(myVariable.length);
+console.log(myVariable.charAt(0));
+console.log(myVariable.indexOf("is"));
+console.log(myVariable.lastIndexOf("is"));
+console.log(myVariable.slice(5,8));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.includes("doc"));
+console.log(myVariable.split(" "));
+
+//Number
+const myNumber = 33;
+const myFloat= 33.01;
+const myVar="1266.4545325eer";
+console.log(myNumber);
+console.log(myNumber===myFloat);
+console.log(Number(myVar));
+console.log(Number("tt")+34);
+console.log(Number(true)+34); 
+console.log(Number.isInteger(45)); 
+console.log(Number.parseFloat(myVar).toFixed(3)); 
+console.log(myFloat.toString()); 
+console.log(Number.isNaN(67e34)); 
+console.log(Number.isNaN("SDS")); 
+
+
+
