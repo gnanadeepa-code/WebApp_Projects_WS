28b56ffc6163d715c707450eced0a327c116ef7d
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-10 11:15:38 +0530

    click event

--- React_Project1/project1/src/Content.js
+++ React_Project1/project1/src/Content.js
@@ -6,7 +6,20 @@ const Content = () => {
   function handleNameChange() {
     const names = ["Earn", "Grow", "Give"];
     const num = Math.floor(Math.random() * 3);
+    console.log(`${names[num]} is returned`);
     return names[num];
+  }
+
+  const handleClick = () => {
+    console.log("Button is clicked");
+  }
+  const handleClick2 = (name) => {
+    console.log(`The technology used is ${name}`);
+  }
+  const handleClick3 = (event) => {
+    console.log(event);
+    console.log(event.target);
+    console.log(event.target.innerText);
   }
   return (
     <main className='App-main'>
@@ -14,6 +27,13 @@ const Content = () => {
       {/*Here the curly braces are used to treat the var name as js var */}
       {/*React cannot take objects,boolean values as child*/}
       <p>Let's {handleNameChange()} Money</p>
+      {/* The above function will be called when the page is loading */}
+      <button onClick={handleClick}> Click Me</button>
+      {/* <button onClick={handleClick()}> Click Me</button> = handleClick() -> if called like this when page is rendering itself the click event will be generated.This is wrong don't call like this.*/}
+      <p><button onClick={() => handleClick2('React')}> Which Technology?</button></p>
+      {/* arrow function is used to call the function with parameter,so that the action takes place only when the button is clicked unlike page rendering itself*/}
+      <p><button onClick={(event) => handleClick3(event)}> Passing event in a function</button></p>
+      {/* arrow function is used to pass the event itself as an object to perform some operations with its event.*/}
     </main>
   );
 };
