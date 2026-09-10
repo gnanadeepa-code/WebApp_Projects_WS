babde2db7e0f7ae58f729c635f94583a622392fe
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-10 18:55:23 +0530

    useState

--- React_Project1/project1/src/Content.js
+++ React_Project1/project1/src/Content.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 const Content = () => {
   const name = "Deepa";
@@ -12,17 +13,27 @@ const Content = () => {
 
   const handleClick = () => {
     console.log("Button is clicked");
-  }
+  };
   const handleClick2 = (name) => {
     console.log(`The technology used is ${name}`);
-  }
+  };
   const handleClick3 = (event) => {
     console.log(event);
     console.log(event.target);
     console.log(event.target.innerText);
+  };
+
+  const [count, setCount] = useState(99); //99 is the default value. useState func returns 2 values as array
+
+  function incrementFunction() {
+    setCount(count + 1);
   }
+  function decrementFunction() {
+    setCount(count - 1);
+  }
+
   return (
-    <main className='App-main'>
+    <main className="App-main">
       <p>Hi {name}</p>{" "}
       {/*Here the curly braces are used to treat the var name as js var */}
       {/*React cannot take objects,boolean values as child*/}
@@ -30,10 +41,25 @@ const Content = () => {
       {/* The above function will be called when the page is loading */}
       <button onClick={handleClick}> Click Me</button>
       {/* <button onClick={handleClick()}> Click Me</button> = handleClick() -> if called like this when page is rendering itself the click event will be generated.This is wrong don't call like this.*/}
-      <p><button onClick={() => handleClick2('React')}> Which Technology?</button></p>
+      <p>
+        <button onClick={() => handleClick2("React")}>
+          {" "}
+          Which Technology?
+        </button>
+      </p>
       {/* arrow function is used to call the function with parameter,so that the action takes place only when the button is clicked unlike page rendering itself*/}
-      <p><button onDoubleClick={(event) => handleClick3(event)}> Passing event in a function</button></p>
+      <p>
+        <button onDoubleClick={(event) => handleClick3(event)}>
+          {" "}
+          Passing event in a function
+        </button>
+      </p>
       {/* arrow function is used to pass the event itself as an object to perform some operations with its event.*/}
+      <p>
+        <button onClick={decrementFunction}>-</button>
+        <span>{count}</span>
+        <button onClick={incrementFunction}>+</button>
+      </p>
     </main>
   );
 };
