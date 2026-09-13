8c1af3da05abe0fc3987bd4ec39ffe1939cd5aec
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-13 19:42:02 +0530

    rendering list

--- React_Project2/React-App/src/Product.jsx
+++ React_Project2/React-App/src/Product.jsx
@@ -1,12 +1,42 @@
-//Props with destructuring mtd - widely used since it is cleaner, no dots required. 
-export const Product = ({title, price, inStock, categories}) => {
+//Props with destructuring mtd - widely used since it is cleaner, no dots required.
+export const Product = ({ title, price, inStock, categories }) => {
+  const productList = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 50000,
+    },
+    {
+      id: 2,
+      name: "Phone",
+      price: 20000,
+    },
+    {
+      id: 3,
+      name: "Tablet",
+      price: 30000,
+    },
+  ];
+
+  const productElts = productList.filter((product) => {
+    return product.price >10000;
+  }).map((product) => {
+    return (
+      <div key= {product.id}>
+        <h3>{product.name}</h3>
+        <p>Price : Rs.{product.price}</p>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h3>{title}</h3>
       <p>Price: Rs.{price}</p>
       <p>In Stock: {inStock ? "Yes" : "No"}</p>
-      <p>Categories: {categories ? categories.join(","): ""}</p> 
-      {/* <p>Categories: {categories?.join(", ")}</p>  */}
+      <p>Categories: {categories ? categories.join(",") : ""}</p>
+
+      {productElts}
     </div>
   );
 };
@@ -18,7 +48,8 @@ export const Product1 = (props) => {
       <h3>{props.title}</h3>
       <p>Price: Rs.{props.price}</p>
       <p>In Stock: {props.inStock ? "Yes" : "No"}</p>
-      <p>Categories:{props.categories.join(", ")}</p> {/* Join is used to join the array elts with comma separated */}
+      <p>Categories:{props.categories.join(", ")}</p>{" "}
+      {/* Join is used to join the array elts with comma separated */}
     </div>
   );
 };
