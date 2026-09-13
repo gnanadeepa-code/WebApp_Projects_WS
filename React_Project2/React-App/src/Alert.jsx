ab1fa38843e8495e932ffe35bd4303c6c49968a5
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-13 20:52:24 +0530

    css module style

--- React_Project2/React-App/src/Alert.jsx
+++ React_Project2/React-App/src/Alert.jsx
@@ -1,8 +1,8 @@
-import "./Alert.css";
+import styles from "./Alert.module.css";
 
-//Eventhough we import Alert.css in this file alone, but it is taken globally, so when the className = error is given in someother file it will pickup from this alert .error class which conflicts, so the 3rd approach css modules is best for solving this styling issues
+//Eventhough we import Alert.css in this file alone, but it is taken globally, so when the className = error is given in someother file it will pickup from this alert .error class which conflicts, so the 3rd approach css modules is best for solving this styling issues. change the file name as Alert.module.css
 export const Alert = ({children, type= "success"}) => {
-    return <div className={`alert ${type}`}>{children}</div>;
+    return <div className={`${styles.alert} ${styles[type]}`}>{children}</div>;
     /* return <div style={{
         backgroundColor: type === "success" ? "lightblue" : "lightgoldenrodyellow",
         color: "black",
