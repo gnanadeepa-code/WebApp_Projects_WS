f12d1dcd995384b6056e405244bfc2d67e2610be
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-13 18:26:58 +0530

    conditional rendering

--- React_Project2/React-App/src/Course.jsx
+++ React_Project2/React-App/src/Course.jsx
@@ -1,12 +1,13 @@
 //Below function set's default values to props. when any of the prop values are not passed it will pickup values from this default values
 function Course({ image,
   show=true, name = "HTML", desc = "Web language" }) {
-  if (show ) { /* Conditional rendering */
+  if (show ) { /* Conditional rendering using If stmt. We can use ternery operator. && is used to render something only when the condition is true otherwise none*/
     return (
       <div className="card">
         <img src={image} alt="Image" />
-        <h3>{name}</h3>
-        <p>{desc}</p>
+        <h3>{name ? name : "Guest"}</h3> 
+        <p>{desc ? desc : "Later"}</p>
+         <p>{show &&  <span> {"\u2605"}</span>}</p>
       </div>
     );
   }else{
