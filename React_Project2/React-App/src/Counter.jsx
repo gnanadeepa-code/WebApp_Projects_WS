3e12d2e9090835ad24272b80e79215d56f995dd3
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-14 21:49:20 +0530

    Batch updates

--- React_Project2/React-App/src/Counter.jsx
+++ React_Project2/React-App/src/Counter.jsx
@@ -13,7 +13,13 @@ export const Counter = () => {
     return 0;
   }); //This fucntion is called only during the initial render and not during each re-render.This is called as Lazy initialization, where it is useful for expensive initial computation like reading values form storage, fetching data from api
 
+  const [name, setName] = useState("");
+  const [isActive, setIsActive] = useState(false);
+
+
   console.log("Counter component rendered with count:", count);
+
+
 
   //In below code the count in after setcount also remains 0 since the State is a snapshot , its value remain 0 even after the setcount function execution. But if based on previous state's value if next action needs to be performed, then we pass a function to the setter instead of a value.
   const handleClick = () => {
@@ -24,8 +30,22 @@ export const Counter = () => {
         console.log("Updated setcount:",prev);
         return prev +5}); //React also queues function but after event handler finishes, it updates the current value with prev updated value unlike passing value as above
     console.log("After setCount prevstate:", count);
+    setCount((prev) => {
+        console.log("Updated setcount:",prev);
+        return prev +10});
+
+    setName("Updated");
+    setIsActive(true);
+    //Even though we have 5 states above React waits until event handler finishes, gathers all state updates and applies them in one render instead of 5 separate renders to load the state. THis is called Batching used for smart optimization.
 
   };
 
-  return <button onClick={handleClick}>Count: {count} </button>;
+  return (
+    <div>
+        <p>Count: {count}</p>
+        <p>Name: {name}</p>
+        <p>Active:{isActive ? "Yes" : "No"}</p>
+        <button onClick={handleClick}>Update all 3</button>;
+    </div>
+  );
 };
