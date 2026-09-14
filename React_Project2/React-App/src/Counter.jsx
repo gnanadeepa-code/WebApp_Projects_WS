3844f34d48219aa058521f055c617eac51839c70
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-14 21:27:48 +0530

    State snapshot

--- React_Project2/React-App/src/Counter.jsx
+++ React_Project2/React-App/src/Counter.jsx
@@ -1,24 +1,31 @@
-import {useState} from "react"
+import { useState } from "react";
 
-export const Counter =() => {
-    //State is a components memory. It is spl data that, 1. Triggers a re-render when it changes (solving screen update problem) 2.Persists btwn renders (solving component's local variable reset problem when re-rendering)
-    //Hooks are spl functions that let you hook into React features
-    //useState hook is used for managing state
-        
-    //currentValue,setterFunction = useState(initialValue) . initialValue can be passed as a function also.
-   // const [count,setCount] = useState(0); //returns array with 2 items
+export const Counter = () => {
+  //State is a components memory. It is spl data that, 1. Triggers a re-render when it changes (solving screen update problem) 2.Persists btwn renders (solving component's local variable reset problem when re-rendering)
+  //Hooks are spl functions that let you hook into React features
+  //useState hook is used for managing state
 
-   const [count,setCount] = useState(() => {
+  //currentValue,setterFunction = useState(initialValue) . initialValue can be passed as a function also.
+  // const [count,setCount] = useState(0); //returns array with 2 items
+
+  const [count, setCount] = useState(() => {
     console.log("Initial state funciton called");
     return 0;
-   }); //This fucntion is called only during the initial render and not during each re-render.This is called as Lazy initialization, where it is useful for expensive initial computation like reading values form storage, fetching data from api
+  }); //This fucntion is called only during the initial render and not during each re-render.This is called as Lazy initialization, where it is useful for expensive initial computation like reading values form storage, fetching data from api
 
-    console.log("Counter component rendered with count:",count);
-    const handleClick =() =>{
-        setCount(count+1);
-    };
+  console.log("Counter component rendered with count:", count);
 
-    return (
-        <button onClick={handleClick}>Count: {count} </button>
-    )
-}
+  //In below code the count in after setcount also remains 0 since the State is a snapshot , its value remain 0 even after the setcount function execution. But if based on previous state's value if next action needs to be performed, then we pass a function to the setter instead of a value.
+  const handleClick = () => {
+    console.log("Before setCount:", count);
+    setCount(count + 1); //React takes the value we pass in and queues it to set count to 1
+    console.log("After setCount:", count);
+    setCount((prev) => {
+        console.log("Updated setcount:",prev);
+        return prev +5}); //React also queues function but after event handler finishes, it updates the current value with prev updated value unlike passing value as above
+    console.log("After setCount prevstate:", count);
+
+  };
+
+  return <button onClick={handleClick}>Count: {count} </button>;
+};
--- React_Project2/React-App/src/LoginCard.jsx
+++ React_Project2/React-App/src/LoginCard.jsx
@@ -1,8 +1,10 @@
 import {useState} from "react";
 
 export const LoginCard = () => {
-    //Golden rules of hooks-- 1. Only call hooks at the top level of component function, not inside loops,conditions,nested functions, try/catch blocks, event handlers, regular js functions 2. Only call hooks from React functions ie. from React components, custom hooks 3. THe order of hooks matters since React uses internal mapping for the below 2 useState hooks in slots (doesn't uses its name), if anyone is mentioned inside the conditional if stmt, then only 1 useState will be available where react confuses about the 2nd missing one 4. Hooks shd be placed at the starting of the function don't use it after the early return stmt
+    //Golden rules of hooks-- 1. Only call hooks at the top level of component function, not inside loops,conditions,nested functions, try/catch blocks, event handlers, regular js functions 2. Only call hooks from React functions ie. from React components, custom hooks 3. THe order of hooks matters since React uses internal mapping for the below 2 useState hooks in slots (doesn't uses its name), if anyone is mentioned inside the conditional if stmt, then only 1 useState will be available where react confuses about the 2nd missing one 4. Hooks shd be placed at the starting of the function don't use it after the early return stmt.
 
+    //In React, state updates are not immediate, first trigger phase where snapshot of state is maintained, once the function is fully completed then only it enters into render phase,variables value never changes within render, finally commit phase is to update the ui component
+    
     //using boolean value datatype for useState
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
