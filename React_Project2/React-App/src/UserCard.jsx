54159bb3d385a21cfbf431e533b3391bb2089d86
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-17 13:41:22 +0530

    use API for context

--- React_Project2/React-App/src/UserCard.jsx
+++ React_Project2/React-App/src/UserCard.jsx
@@ -1,46 +1,55 @@
-import { UserInfo } from "./UserInfo"
-import { ActionButton } from "./ActionButton"
-import { useContext } from "react"
-import { UserContext } from "./UserContext"
+import { UserInfo } from "./UserInfo";
+import { ActionButton } from "./ActionButton";
+import { use, useContext } from "react";
+import { UserContext } from "./UserContext";
 
 //In this function we are using spread property ... to include all the props from UserInfo without listing them individually
 export const UserCard = (props) => {
+  //To use the context import useContext hook and the UserContext component.We don't need props to send values from parent to the nested child where it gets messy.
+  // const user = useContext(UserContext);
+  /*  const {user, setUser} = useContext(UserContext); */
 
-    //To use the context import useContext hook and the UserContext component.We don't need props to send values from parent to the nested child where it gets messy. 
-   // const user = useContext(UserContext);
-    const {user, setUser} = useContext(UserContext);
+  //we can use, the use API in react to read values from context instead of useContext hook as below,since use API is not a hook it does not need to follow the rules of hooks like, 1.Hooks must be called at the top level of component 2.Can't call them inside conditions, loops or early returns 3.Every render has to call hooks in the same order
 
-    const toggleTheme = () => {
-        setUser({
-            ...user,
-            theme:user.theme === "dark" ? "light" : "dark",
-        });
-    };
+  const isLoading = false;
+  if (isLoading) {
+    return <div>Loading user data...</div>;
+  }
+  /*  const { user, setUser } = useContext(UserContext); */
+  const { user, setUser } = use(UserContext);
 
-    const handleSaveDetails = () => {
-        alert("User Details saved");
-    }
-    return(
-        <div>
-            <h2>User Details</h2>
-            <UserInfo {...props} />
-             <ActionButton text="Save Details" onClick={handleSaveDetails}/> {/* here the event handling is passed as props */}
-             <h3>Using Context</h3>
-             <p>Name: {user.name}</p>
-             <p>ROle: {user.role}</p>
-             <p>Theme: {user.theme}</p>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-        </div>
-    )
-}
+  const toggleTheme = () => {
+    setUser({
+      ...user,
+      theme: user.theme === "dark" ? "light" : "dark",
+    });
+  };
+
+  const handleSaveDetails = () => {
+    alert("User Details saved");
+  };
+  return (
+    <div>
+      <h2>User Details</h2>
+      <UserInfo {...props} />
+      <ActionButton text="Save Details" onClick={handleSaveDetails} />{" "}
+      {/* here the event handling is passed as props */}
+      <h3>Using Context</h3>
+      <p>Name: {user.name}</p>
+      <p>ROle: {user.role}</p>
+      <p>Theme: {user.theme}</p>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+  );
+};
 
 //In this function we are using rest property ... to collect rest of the props from UserInfo without listing them individually except id prop
-export const UserCard1 = ({id, ...rest}) => {
-    return(
-        <div>
-            <h2>User Details</h2>
-            <p>User Id: {id}</p>
-            <UserInfo {...rest} />
-        </div>
-    )
-}
+export const UserCard1 = ({ id, ...rest }) => {
+  return (
+    <div>
+      <h2>User Details</h2>
+      <p>User Id: {id}</p>
+      <UserInfo {...rest} />
+    </div>
+  );
+};
