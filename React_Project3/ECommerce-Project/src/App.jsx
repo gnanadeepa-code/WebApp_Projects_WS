5e750d63959946f8ad6a05bb18f4c41a6387cc0d
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-21 13:13:41 +0530

    moved tracking page into react

--- React_Project3/ECommerce-Project/src/App.jsx
+++ React_Project3/ECommerce-Project/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<OrdersPage />} />
     </Routes>
   );
 }
