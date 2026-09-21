93e5a094aadca8372844eeaeb748191b7ab13dbd
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-21 12:48:08 +0530

    move checkoutpage into React

--- React_Project3/ECommerce-Project/src/App.jsx
+++ React_Project3/ECommerce-Project/src/App.jsx
@@ -1,13 +1,14 @@
 import { HomePage } from "./pages/HomePage";
 import { Route, Routes } from "react-router";
 import "./App.css";
+import { CheckoutPage } from "./pages/CheckoutPage";
 
 function App() {
   //Route is used to move from page to another. For that we need to separately install npm react-router in our project
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="checkout" element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   );
 }
