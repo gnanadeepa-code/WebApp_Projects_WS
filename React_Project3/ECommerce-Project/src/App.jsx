9bf098514451da9f9343779a9572e8858f3c0f1f
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-21 13:08:52 +0530

    moved orders page into react

--- React_Project3/ECommerce-Project/src/App.jsx
+++ React_Project3/ECommerce-Project/src/App.jsx
@@ -2,6 +2,7 @@ import { HomePage } from "./pages/HomePage";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
 
 function App() {
   //Route is used to move from page to another. For that we need to separately install npm react-router in our project
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   );
 }
