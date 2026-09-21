780c1b27f0d182a7215f300b53f0688908e6bf2d
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2026-09-21 13:40:20 +0530

    made header as a separate component and use link from router

--- React_Project3/ECommerce-Project/src/App.jsx
+++ React_Project3/ECommerce-Project/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
 
 function App() {
   //Route is used to move from page to another. For that we need to separately install npm react-router in our project
@@ -11,7 +12,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
-      <Route path="tracking" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   );
 }
